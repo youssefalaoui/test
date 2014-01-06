bd5c8b44ea556b0be19a76ff81a037181eab0dc4
youssef <yousseff.alaoui@gmail.com>
2014-01-06 13:15:01 +0100

    Initial creation with a very simple sample.
    Signed-off-by: youssef <yousseff.alaoui@gmail.com>

---
+++ test/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Created on 6 Jan 2014
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+ echo "this is a first test my friends !";
+?>
